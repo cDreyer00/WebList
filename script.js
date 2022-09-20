fd4e1fe46379dcb64453a9edcaf4770742933ff3
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2022-09-20 18:15:26 -0300

    removing and edditing itens

--- script.js
+++ script.js
@@ -17,33 +17,49 @@ window.addEventListener('load', () => {
         task_el.classList.add("task");
 
         let task_content_el = document.createElement("div");
-        task_content_el.classList.add("content");              
-        
+        task_content_el.classList.add("content");
+
         let taks_input_el = document.createElement("input");
         taks_input_el.classList.add("text");
         taks_input_el.type = "text";
         taks_input_el.value = task;
         taks_input_el.setAttribute("readonly", "readonly");
-        
+
         task_content_el.appendChild(taks_input_el);
 
-        let task_actions_el = document.createElement("div");        
+        let task_actions_el = document.createElement("div");
         task_actions_el.classList.add("actions");
 
         let task_actions_edit_el = document.createElement("button");
         task_actions_edit_el.classList.add("edit");
-        task_actions_edit_el.innerHTML = "edit";
+        task_actions_edit_el.innerHTML = "EDIT";
 
-        let task_actions_delete_el = document.createElement("button");    
+        let task_actions_delete_el = document.createElement("button");
         task_actions_delete_el.classList.add("delete");
-        task_actions_delete_el.innerHTML = "delete";
+        task_actions_delete_el.innerHTML = "DELETE";
 
         task_actions_el.appendChild(task_actions_edit_el);
         task_actions_el.appendChild(task_actions_delete_el);
-        task_el.appendChild(task_content_el);        
-        task_el.appendChild(task_actions_el);  
+        task_el.appendChild(task_content_el);
+        task_el.appendChild(task_actions_el);
         list_el.appendChild(task_el);
 
         input.value = "";
-    })
-})
+
+        task_actions_edit_el.addEventListener('click', () => {
+            if (task_actions_edit_el.textContent.toLowerCase() == "edit") {
+                taks_input_el.removeAttribute("readonly");
+                taks_input_el.focus();
+                task_actions_edit_el.textContent = "SAVE";
+            }
+            else {
+                taks_input_el.setAttribute("readonly", "readonly");
+                task_actions_edit_el.textContent = "EDIT";
+            }
+        })
+
+        task_actions_delete_el.addEventListener('click', ()=>{
+            list_el.removeChild(task_el);
+        });
+    });
+});
